e496c7c4567742c562774a72885c598bebf00027
vin-devs <vmutuku706@gmail.com>
2025-12-18 16:09:18 +0300

    Updated some files

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useLoginMutation } from "../../redux/api/usersApiSlice";
 import { setCredentials } from "../../redux/features/auth/authSlice";
 import { toast } from "react-toastify";
+import Loader from "../../components/Loader";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -24,13 +25,25 @@ const Login = () => {
     }
   }, [navigate, redirect, userInfo]);
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await login({ email, password }).unwrap();
+      console.log(res);
+      dispatch(setCredentials({ ...res }));
+    } catch (error) {
+      toast.error(error?.data?.message || error.message);
+    }
+  };
+
   return (
     <div>
       <section className="pl-[10rem] flex flex flex-wrap">
         <div className="mr-[4rem] mt-[5rem]">
           <h1 className="text-2xl font-semibold mb-4">Sign In</h1>
 
-          <form className="container w-[40rem]">
+          <form onSubmit={submitHandler} className="container w-[40rem]">
             <div className="my-[2rem]">
               <label htmlFor="email" className="block text-sm font-medium">
                 Email Address
@@ -45,7 +58,7 @@ const Login = () => {
             </div>
 
             <div className="my-[2rem]">
-              <label htmlFor="email" className="block text-sm font-medium">
+              <label htmlFor="password" className="block text-sm font-medium">
                 Password
               </label>
               <input
@@ -53,7 +66,7 @@ const Login = () => {
                 id="password"
                 className="mt-1 p-2 border rounded w-full"
                 value={password}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
@@ -67,6 +80,17 @@ const Login = () => {
 
             {isLoading && <Loader />}
           </form>
+          <div className="mt-4">
+            <p className="text-dark">
+              New Customer ?{" "}
+              <Link
+                to={redirect ? `/register?redirect=${redirect}` : "/register"}
+                className="text-pink-500 hover:underline"
+              >
+                Register
+              </Link>
+            </p>
+          </div>
         </div>
       </section>
     </div>
